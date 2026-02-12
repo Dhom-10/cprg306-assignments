eaf9e9ff590d7f7263964b190216c50fc2054d99
Abulrahman Nasser <dhom.10h@gmail.com>
2026-02-12 14:50:01 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="p-6">
-      <h1 className="text-2xl font-bold mb-4">CPNT 217</h1>
+      <h1 className="text-2xl font-bold mb-4">CPRG 306: Web Development 2 - Assignments</h1>
 
       <ul className="list-disc pl-6 space-y-2">
         <li>
@@ -21,7 +21,7 @@ export default function Home() {
             Week 4
           </Link>
         </li>
-        
+
         <li>
           <Link className="text-blue-600 underline" href="/week-5">
             Week 5
